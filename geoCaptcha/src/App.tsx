ade2c283df1aa375e7cd1bac0fb475816145f3df
Amikola <atte.ikola@gmail.com>
2025-08-22 17:15:22 +0300

    Css is my passion

--- geoCaptcha/src/App.tsx
+++ geoCaptcha/src/App.tsx
@@ -1,13 +1,62 @@
+
 import Captcha from './modules/Captcha'
+import "./App.css";
+
+import { useState } from 'react';
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [loggedIn, showCaptcha] = useState(false);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+  
+    if (username && password) {
+
+      showCaptcha(true);
+    }
+  };
 
   return (
-    <>
-      <p>Skibidi toilet</p>
-      <Captcha />
-    </>
-  )
+    <div className="app-Container">
+       <h1 className='Title'>Cool as hell App</h1>
+    <div className="app">
+     
+      {!loggedIn ? (
+        <form onSubmit={handleSubmit} className="login-form">
+          <div>
+            <label>
+              Username: 
+              <input
+                type="text"
+                value={username}
+                onChange={e => setUsername(e.target.value)}
+                required
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              Password: 
+              <input
+                type="password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                required
+              />
+            </label>
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      ) : (
+        <div>
+          <Captcha />
+        </div>
+      )}
+    </div>
+    </div>
+  );
 }
 
 export default App
