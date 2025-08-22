4acc5d39db79affcb6b5f97f2dd3c4bf06bd65a7
toivoPal <toivopalmen@gmail.com>
2025-08-22 19:29:48 +0300

    javascriptify interface

--- vibe_folder/db_connection.py
+++ vibe_folder/db_connection.py
@@ -1,15 +1,28 @@
+from flask import Flask, jsonify, request
 from pathlib import Path
 import pandas as pd
 
+app = Flask(__name__)
+
 def get_captcha():
     df = pd.read_csv(Path(__file__).parent.parent.resolve() / "database.txt")
-    return df.iloc[0]
+    return df.iloc[0].to_dict()
 
 def check_captcha(id, answer) -> bool:
     df = pd.read_csv(Path(__file__).parent.parent.resolve() / "database.txt")
     return df.loc[df["id"] == id, "captcha"].eq(answer).any()
 
+@app.route("/get_captcha", methods=["GET"])
+def api_get_captcha():
+    return jsonify(get_captcha())
+
+@app.route("/check_captcha", methods=["POST"])
+def api_check_captcha():
+    data = request.json
+    id = data.get("id")
+    answer = data.get("answer")
+    result = check_captcha(id, answer)
+    return jsonify({"result": result})
+
 if __name__ == "__main__":
-    print(get_captcha())
-    print(f"Should be true: {check_captcha(1,'bruh-face')}")
-    print(f"Should be false: {check_captcha(1,'bruh')}")
+    app.run(debug=True)
